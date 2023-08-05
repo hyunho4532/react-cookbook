b17d0a176a72dbbaa19300c17f1dfe5a4e54536d
hyunho4621 <137243882+hyunho4621@users.noreply.github.com>
2023-08-05 19:00:17 +0900

    2023-08-05 props

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import './App.css';
+import Counter from './Counter';
 import MyHeader from './MyHeader';
 
 function App() {
 
-  let name = "Hyunho4532";
-
-  const style = {
-    App: {
-      backgroundColor: 'black',
-    },
-    h2: {
-      color: "red",
-    },
-    bold_text: {
-      color: "green",
-    }
-  }
-
-  const number = 5;
 
   return (
-    <div style={style.App}>
+    <div>
       <MyHeader />
-      <header className="App-header">
-        <h2 style={style.h2}>안녕 리액트</h2>
-        <b style={style.bold_text}>
-          {number}는 : {number % 2 === 0 ? "짝수" : "홀수" }
-        </b>
-      </header>
+      <Counter />
     </div>
   );
 }
