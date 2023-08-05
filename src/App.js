5461d061db6dec214e5141cf0c72c6e0dcf51eef
hyunho4621 <137243882+hyunho4621@users.noreply.github.com>
2023-08-05 18:12:59 +0900

    2023-08-05 JSX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
+import MyHeader from './MyHeader';
 
 function App() {
 
   let name = "Hyunho4532";
 
+  const style = {
+    App: {
+      backgroundColor: 'black',
+    },
+    h2: {
+      color: "red",
+    },
+    bold_text: {
+      color: "green",
+    }
+  }
+
+  const number = 5;
+
   return (
-    <div className="App">
+    <div style={style.App}>
+      <MyHeader />
       <header className="App-header">
-        <h2>안녕 리액트, {name}</h2>
+        <h2 style={style.h2}>안녕 리액트</h2>
+        <b style={style.bold_text}>
+          {number}는 : {number % 2 === 0 ? "짝수" : "홀수" }
+        </b>
       </header>
     </div>
   );
---
+++ src/MyHeader.js
@@ -0,0 +1,5 @@
+const MyHeader = (name) => {
+    return <header>헤더</header>
+};
+
+export default MyHeader;
