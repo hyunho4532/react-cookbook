4888e0fdf675590917d8e0d4653afe382931b0ec
hyunho4621 <137243882+hyunho4621@users.noreply.github.com>
2023-08-05 22:39:54 +0900

    2023-08-05 리스트 렌더링 (조회)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id: 1,
+    author: "Hyunho4532",
+    content: "Hello 1",
+    emotion: 5,
+    create_date: new Date().getTime(),
+  },
+
+  {
+    id: 2,
+    author: "홍길동",
+    content: "Hello 2",
+    emotion: 4,
+    create_date: new Date().getTime(),
+  },
+
+  {
+    id: 3,
+    author: "아무개",
+    content: "Hello 3",
+    emotion: 2,
+    create_date: new Date().getTime(),
+  },
+]
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
